eaefa2161c4575e8a1dce4e176f06ff8edbca0a9
tmga0502 <tmga0502$gmail.com>
2023-05-03 19:26:43 +0900

    JSでのAPI処理をやめたことに伴うmain controllerの変更

--- src/app/Http/Controllers/ZukanController.php
+++ src/app/Http/Controllers/ZukanController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pokemon;
 use Illuminate\Http\Request;
 
 class ZukanController extends Controller
 {
   public function index(){
-    return view('zukan.index');
+    $pokemons = Pokemon::all();
+    return view('zukan.index', compact('pokemons'));
   }
 
   public function show($id){
