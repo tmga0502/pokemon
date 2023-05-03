986eea1aa6fdff38308689a6faa916dfdde5acb9
tmga0502 <tmga0502$gmail.com>
2023-05-04 03:10:28 +0900

    TypeSeederの修正

--- src/database/seeders/TypeSeeder.php
+++ src/database/seeders/TypeSeeder.php
@@ -20,75 +20,75 @@ class TypeSeeder extends Seeder
       Type::insert([
         [
           'name' => 'ノーマル',
-          'icon_path' => './assets/img/pokemon_types/normal.svg'
+          'icon_path' => 'assets/img/pokemon_types/normal.svg'
         ],
         [
           'name' => 'ほのお',
-          'icon_path' => './assets/img/pokemon_types/honoo.svg'
+          'icon_path' => 'assets/img/pokemon_types/honoo.svg'
         ],
         [
           'name' => 'みず',
-          'icon_path' => './assets/img/pokemon_types/mizu.svg'
+          'icon_path' => 'assets/img/pokemon_types/mizu.svg'
         ],
         [
           'name' => 'くさ',
-          'icon_path' => './assets/img/pokemon_types/kusa.svg'
+          'icon_path' => 'assets/img/pokemon_types/kusa.svg'
         ],
         [
           'name' => 'でんき',
-          'icon_path' => './assets/img/pokemon_types/denki.svg'
+          'icon_path' => 'assets/img/pokemon_types/denki.svg'
         ],
         [
           'name' => 'こおり',
-          'icon_path' => './assets/img/pokemon_types/koori.svg'
+          'icon_path' => 'assets/img/pokemon_types/koori.svg'
         ],
         [
           'name' => 'かくとう',
-          'icon_path' => './assets/img/pokemon_types/kakutou.svg'
+          'icon_path' => 'assets/img/pokemon_types/kakutou.svg'
         ],
         [
           'name' => 'どく',
-          'icon_path' => './assets/img/pokemon_types/doku.svg'
+          'icon_path' => 'assets/img/pokemon_types/doku.svg'
         ],
         [
           'name' => 'じめん',
-          'icon_path' => './assets/img/pokemon_types/zimen.svg'
+          'icon_path' => 'assets/img/pokemon_types/zimen.svg'
         ],
         [
           'name' => 'ひこう',
-          'icon_path' => './assets/img/pokemon_types/hikou.svg'
+          'icon_path' => 'assets/img/pokemon_types/hikou.svg'
         ],
         [
           'name' => 'エスパー',
-          'icon_path' => './assets/img/pokemon_types/esper.svg'
+          'icon_path' => 'assets/img/pokemon_types/esper.svg'
         ],
         [
           'name' => 'むし',
-          'icon_path' => './assets/img/pokemon_types/mushi.svg'
+          'icon_path' => 'assets/img/pokemon_types/mushi.svg'
         ],
         [
           'name' => 'いわ',
-          'icon_path' => './assets/img/pokemon_types/iwa.svg'
+          'icon_path' => 'assets/img/pokemon_types/iwa.svg'
         ],
         [
           'name' => 'ゴースト',
-          'icon_path' => './assets/img/pokemon_types/ghost.svg'
+          'icon_path' => 'assets/img/pokemon_types/ghost.svg'
         ],
         [
           'name' => 'ドラゴン',
-          'icon_path' => './assets/img/pokemon_types/dragon.svg'
+          'icon_path' => 'assets/img/pokemon_types/dragon.svg'
         ],
         [
           'name' => 'あく',
-          'icon_path' => './assets/img/pokemon_types/aku.svg'
+          'icon_path' => 'assets/img/pokemon_types/aku.svg'
         ],
         [
           'name' => 'はがね',
-          'icon_path' => './assets/img/pokemon_types/hagane.svg'
+          'icon_path' => 'assets/img/pokemon_types/hagane.svg'
         ],
         [
           'name' => 'フェアリー',
-          'icon_path' => './assets/img/pokemon_types/fairy.svg'
+          'icon_path' => 'assets/img/pokemon_types/fairy.svg'
         ],
       ]);
     }
